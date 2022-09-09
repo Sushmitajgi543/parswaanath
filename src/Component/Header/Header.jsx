f44ec5ec1bf32d0def1eb184344f498a508a1027
Sushmita Kumari <sushmitajgi543@gmail.com>
2022-09-09 12:21:25 +0530

    add page

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -3,7 +3,7 @@ import Container from '../Container/Container'
 import './Header.scss'
 import logo from "../../Assets/Image/logo.png"
 import menu from "../../Assets/Image/menu.png"
-
+import { Link } from 'react-router-dom';
 function Header() {
   return (
     <div className='Header'>
@@ -13,8 +13,9 @@ function Header() {
                     <img src={menu} alt="" />
                 </span>
                 <ul>
+                    
                     <li><a href="">Products</a></li>
-                    <li><a href="">About us</a></li>
+                    <li><Link to="/about-us"><a href="">About us</a></Link></li>
                     <li><a href="">Contact</a></li>
                     
                 </ul>
